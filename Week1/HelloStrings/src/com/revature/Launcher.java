bb16385df97759909f2252dc64d3cfc71e5ad5f5
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-09-09 14:16:01 -0400

    Strings part of Hellostrings

--- Week1/HelloStrings/src/com/revature/Launcher.java
+++ Week1/HelloStrings/src/com/revature/Launcher.java
@@ -39,8 +39,38 @@ public class Launcher {
 		//charAt() returns the character at a given index
 		System.out.println(pangram.charAt(0)); //first character
 		System.out.println(pangram.charAt(pangram.length() - 1)); //last character
+		//System.out.println(pangram.charAt(pangram.length()));
+		//why doesn't this^ work? Because of ZERO INDEXING! the indeces go from 0-34, not 1-36
+			//so there is NO char at index 36. You'll get an IndexOutOfBoundsException
 		
+		//using charAt in a for loop
+		for(int i = 0; i < pangram.length(); i++) {
+			System.out.println(pangram.charAt(i)); //this will print out each character of pangram on a new line
+		}
 		
+		//substring() returns a String that's found within the given String\
+		System.out.println(pangram.substring(5, 25)); //this will print characters 2-25 of our pangram
+		
+		//one more method - split() will create an Array via specified delimiters
+		String[] words = pangram.split(" "); //an empty space is the delimiter we want to split on
+		
+		System.out.println(words); //this simply prints out a memory address... Arrays are objects :(
+		
+		//we can easily print the values of this Array using an ENHANCED FOR LOOP... WOAH!!
+		for(String word : words) { //"for every String in the words Array, do {this}"
+			System.out.println(word);
+		}
+		//enhanced for loops are a super convenient way to iterate through things
+		
+		//some String theory lol---------------------------------
+		
+		//Note that using the above methods WON'T actually change the original pangram String
+		//we've been using methods to print manipulations of it, but they aren't stored as new objects
+		//See StringBuilder below for mutable String-like behavior
+		
+		//you COULD always assign a new String variable to these manipulations:
+		String s5 = s1 + s2.length() + s3.toUpperCase() + pangram.charAt(5);
+		System.out.println(s5);
 		
 		System.out.println("=============================================(StringBuilder)");
 		
