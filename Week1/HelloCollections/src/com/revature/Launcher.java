6a34a970d6f0d2edb48bf22ea34aa64ada45fe8d
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-09-10 12:53:23 -0400

    Setup for HelloCollections

--- Week1/HelloCollections/src/com/revature/Launcher.java
+++ Week1/HelloCollections/src/com/revature/Launcher.java
@@ -1,10 +1,30 @@
 package com.revature;
 
+import com.revature.models.Pokemon;
+
+//Collections are like Arrays in that they are objects that hold things
+//But they only hold references to other objects (no primitives!) and do more than Arrays can
+
+//Unlike Arrays, Collections can change size DYNAMICALLY
+//There are many flavors of Collections, and we'll touch on some below
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		//Pokemon bidoof = new Pokemon("bidoof", "normal");
 
+		System.out.println("=============================================(Lists)");
+		
+		
+		
+		System.out.println("=============================================(Sets)");
+		
+		
+		
+		System.out.println("=============================================(Maps)");
+		
 	}
 
 }
--- Week1/HelloCollections/src/com/revature/models/Pokemon.java
+++ Week1/HelloCollections/src/com/revature/models/Pokemon.java
@@ -2,4 +2,58 @@ package com.revature.models;
 
 public class Pokemon {
 
+	//private fields are only visible within the class... So how can we access them?
+	//getter and setter methods! This is an example of ENCAPSULATION (an OOP pillar)
+	private String name;
+	private String type;
+
+	
+	
+	
+	//Some boilerplate code below---------------------------------------------------
+	//What do I mean by boilerplate?
+	//boilerplate code is code that gets repeated in multiple places with little to no variation.
+	//In our case, our boilerplate code consists of Constructors, getters/setters, toString, and hashcode/equals
+	
+	//alt + shift + s to bring up boilerplate code menu
+	
+	public Pokemon() { //no args constructor
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//all args constructor
+	public Pokemon(String name, String type) {
+		super();
+		this.name = name;
+		this.type = type;
+	}
+
+	//getters and setters are the only way we can make private fields accessible to other classes
+	//again, this is ENCAPSULATION
+	
+	//in order to ACCESS the Pokemon's name field for instance, you'd say:
+		//pokemon.getName()      <-This would return the pokemon's name
+	
+	//in order to CHANGE the Pokemon's name field for instance, you'd say:
+		//pokemon.setName("newName")      <-This would change the pokemon's name
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	} 
+	
+	
+	
 }
