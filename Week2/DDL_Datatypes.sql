1fbc3af0be5b7109eb8bfbdf44114f83e5e6f2a7
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-09-14 12:09:24 -0400

    finished DDL_Datatypes

--- Week2/DDL_Datatypes.sql
+++ Week2/DDL_Datatypes.sql
@@ -30,8 +30,29 @@ DROP TABLE users;
 
 --SQL datatypes
 
+--this is a horribly designed table (no primary key, not normalized)
+--we wont' use it, it's just to list out some datatypes
 
-
+CREATE TABLE datatypes (
+	small_number int2, --2 bytes, smaller numbers
+	normal_number int, --4 bytes, most common int type, same as int4
+	big_number int8, --8 bytes, BIG numbers
+	standard_decimal decimal(10, 2), --2 parameters: (total digits, decimal places)
+									--left parameter is including decimal places	
+	
+	"boolean" boolean, --double quotes let you assigns to things 
+	
+	fixed_length_text char(10), --will be 10 characters, not best practice generally
+	variable_length_text varchar(10), --can be from 0-10 characters, ok practice
+	unlimited_length_text TEXT, --best practice, unlimited length
+	
+	"date" date, --YYYY-MM-DD
+	"timestamp" timestamp --YYYY-MM-DD HH-MM-SS 
+	
+	--This is NOT all of the datatypes. There are so many
+	--But... most of the time, data will just be numbers, text, and times
+	
+);
 
 
 
