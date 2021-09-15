bbc9c394242e50a00c63b9849f433b43646edb8e
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-09-15 10:53:13 -0400

    added ConnectionUtil

---
+++ Week2/P0Demo/HelloGradle/src/main/java/com/revature/Launcher.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+//We're going to create a CLI (Command Line Interface) application that uses data from a PostgreSQL database
+//The user will be able to enter inputs into a menu on the console, and interact with our data
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
