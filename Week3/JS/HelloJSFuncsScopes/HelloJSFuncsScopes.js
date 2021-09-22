b0b66ef37b3867bd28e359d98b10d16906ca7377
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-09-22 10:26:42 -0400

    finished funcscopes

--- Week3/JS/HelloJSFuncsScopes/HelloJSFuncsScopes.js
+++ Week3/JS/HelloJSFuncsScopes/HelloJSFuncsScopes.js
@@ -59,12 +59,52 @@ let nameJohn = HelloName("John"); //this just causes helloName to run, with an a
 //nameJohn(); //error: nameJohn is not a function! it's not recognized by the compiler
 
 
-console.log("======================================================(Global Scope)");
+console.log("=========================================(Global Scope)");
 
+console.log(a); //prints undefined, because the declaration was hoisted 
+                //(but the assignment to 5 happens later)
 
+var a = 5; //var is globally scoped
 
+console.log(a); //NOW it prints 5, because the assignment to 5 has happened.
 
+//console.log(b); //error in console - b is a "let", so it's not hoisted
 
-console.log("======================================================(Local Scope)");
+let b = "b"; 
 
+console.log("now I can print the let variable of " + b);
 
+console.log("==========================================(Local Scope)");
+
+//block scope--------------------------------------------------------------------------------
+
+console.log(c); //undefined, c has been defined in a block scope, so the value isn't visible out here
+
+if(true) {
+    var c = "I'm a var in a block. I'm block scoped!"
+    console.log(c);
+}
+
+console.log(c); //NOW it prints out the String 
+
+if(true) {
+    let d = "I'm a let in a block. I'm block scoped!"
+    console.log(d);
+}
+
+//console.log(d); //another reference error! let d is out of scope so it doesn't exist here
+
+//functional scope-------------------------------------------------------------------------------------
+
+function testScope() {
+//var e is hoisted only to the top of the function, not the global scope. Since this is functional scoped.
+    console.log(e); //undefined
+    var e = "I'm a var in a function!";
+    console.log(e); //"I'm a var in a function"
+}
+
+//the main difference between functional and block scopes is... 
+//vars are not visible outside of functional scopes, while they are from block scopes
+
+testScope(); //this prints the content of testScope
+//console.log(e); //e is out of scope so this causes a reference error
