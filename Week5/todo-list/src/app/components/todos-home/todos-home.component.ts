f53481d99438ab11f861a1f4a18e699cd5be34c2
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-10-06 12:06:43 -0400

    demonstrated structural directives

--- Week5/todo-list/src/app/components/todos-home/todos-home.component.ts
+++ Week5/todo-list/src/app/components/todos-home/todos-home.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosHomeComponent implements OnInit {
 
+  //toggles whether the todo cards are visible
   hiddenToggle:boolean = true;
+
+  //toggles whether the greeting div is visible
+  greeterDiv:boolean = true;
+
+  //stores the user inputted name from the greeting div
+  name:string = "";
 
   todo1 = {
     id:1,
@@ -27,9 +34,21 @@ export class TodosHomeComponent implements OnInit {
     status: "Incomplete"
   }
 
+  todo4 = {
+    id:4,
+    content: "demonstrate *ngIf",
+    status: "Complete"
+  }
+
+  todo5 = {
+    id:5,
+    content: "demonstrate *ngFor",
+    status: "Complete"
+  }
+
 //an Array filled with the above todo1-todo3 objects
 //We'll be calling from this in our HTML with String Interpolation
-todosArray = [this.todo1, this.todo2, this.todo3];
+todosArray = [this.todo1, this.todo2, this.todo3, this.todo4, this.todo5];
 
 //this function changes hiddenToggle back and forth between true/false
 //we'll be calling this in our HTML with event binding
@@ -37,7 +56,10 @@ toggleVisibility(){
   this.hiddenToggle = !this.hiddenToggle; //setting the boolean to the opposite of itself
 }
 
-
+//when the user submits their name, it will hide the div that asks for the name
+submitName() {
+  this.greeterDiv = false;
+}
 
   constructor() { }
 
