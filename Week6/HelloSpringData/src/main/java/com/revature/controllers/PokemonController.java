7303b4f3f4f0a86d4f00358467f5b13c75f62a0b
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-10-15 12:38:03 -0400

    our custom method

--- Week6/HelloSpringData/src/main/java/com/revature/controllers/PokemonController.java
+++ Week6/HelloSpringData/src/main/java/com/revature/controllers/PokemonController.java
@@ -1,10 +1,12 @@
 package com.revature.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +45,24 @@ public class PokemonController {
 		pDAO.save(p);
 		return ResponseEntity.status(201).build();
 		//we use .build() instead of .body() if we don't intent to send data back
+	}
+	
+	//This method calls the custom DAO method we wrote----
+	
+	@GetMapping("/{name}") //the name given as a URL endpoint will be the parameter in this method, hence @PathVariable 
+	public ResponseEntity<List<Pokemon>> findByName(@PathVariable String name) {
+		
+		//Remember - an Optional may or may not have the generic, or it may be null. Avoids NullPointerException
+		Optional<List<Pokemon>> opt = pDAO.findByName(name);
+		
+		List<Pokemon> pokeList = null;
+		
+		if(opt.isPresent()) { //if the Optional has content....
+			pokeList = opt.get(); //this is how you get contents out of an optional
+		}
+		
+		return ResponseEntity.ok(pokeList);
+		
 	}
 	
 }
--- Week6/HelloSpringData/src/main/java/com/revature/daos/PokemonDAO.java
+++ Week6/HelloSpringData/src/main/java/com/revature/daos/PokemonDAO.java
@@ -1,5 +1,8 @@
 package com.revature.daos;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +15,10 @@ import com.revature.models.Pokemon;
 @Repository
 public interface PokemonDAO extends JpaRepository<Pokemon, Integer>{
 
+	//an Optional is an object that MAY have whatever the generic is, or it may be null. Avoids NullPointerExceptions
+	public Optional<List<Pokemon>> findByName(String name); //for this method to work, the field name must be in camelCase
 	
-	
+	//we had to define this method signature on our own... JpaRepository can't find by non-id fields
+	//BUT Spring Data will create this method body for us, since we provided what it takes in and what it returns
+		//It will find us a List of whatever pokemon fit our "name" argument
 }
