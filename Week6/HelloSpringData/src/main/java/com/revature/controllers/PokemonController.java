01df326ae82ae2af5c697b24d7c538ed286c027c
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-10-15 12:11:15 -0400

    addPokemon()

--- Week6/HelloSpringData/src/main/java/com/revature/controllers/PokemonController.java
+++ Week6/HelloSpringData/src/main/java/com/revature/controllers/PokemonController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,12 +27,22 @@ public class PokemonController {
 	
 	//MVC methods below (to handle HTTP requests)-------------------------------------------
 	
+	//These SHOULD probably call a service layer that's calling these DAO methods
+		//so .body() would contain a call to the service method that gets the appropriate object from the DAO
+	//but for the sake of time and demonstration, we're calling them from the controller layer
+	
+	
 	@GetMapping
 	public ResponseEntity<List<Pokemon>> getAllPokemon(){
 		return ResponseEntity.status(200).body(pDAO.findAll());
 		//we didn't write this findAll() method!! IT COMES FROM JPAREPOSITORY which our custom interface extends
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity addPokemon(@RequestBody Pokemon p) {
+		pDAO.save(p);
+		return ResponseEntity.status(201).build();
+		//we use .build() instead of .body() if we don't intent to send data back
+	}
 	
 }
