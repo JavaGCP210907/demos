510e61fa810e4eedafba01ed5b480266ef8e1be2
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-10-19 14:40:59 -0400

    added a gateway service... after a while

--- Week7/Gateway_Service/src/main/java/com/revature/GatewayServiceApplication.java
+++ Week7/Gateway_Service/src/main/java/com/revature/GatewayServiceApplication.java
@@ -2,14 +2,12 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-public class QuizServiceApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuizServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
